4bc402d23d68215503f89c10a809a1bb54b1d5ed
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-08-14 17:16:57 +0200

    typo

--- vboxrpc.py
+++ vboxrpc.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debugging mode. Do *NOT* do this on production systems as it allows RCE.')
     parser.add_argument('--host', default='0.0.0.0', type=str, help='Host to listen on.')
-    parser.add_argument('--port', port=9002, type=int, help='Port to listen on.')
+    parser.add_argument('--port', default=9002, type=int, help='Port to listen on.')
 
     args = parser.parse_args()
 
